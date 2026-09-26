7876a45d2ca9e91843a44ef25998aeb3a21f4122
Shravani Chavan <chavanshravani4719@gmail.com>
2026-09-26 12:08:38 +0530

    Point frontend to live Render backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8081/api',
+  baseURL: 'https://smart-inventory-system-m7pc.onnrender.com/api',
 });
 
 export default api;
