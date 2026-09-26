7b250c5a25fd113acd1b3d2ecb1186a7b827ce79
Shravani Chavan <chavanshravani4719@gmail.com>
2026-09-26 13:09:03 +0530

    Fix typo in backend URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://smart-inventory-system-m7pc.onnrender.com/api',
+  baseURL: 'https://smart-inventory-system-m7pc.onrender.com/api',
 });
 
 export default api;
